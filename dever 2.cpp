f89359e6cc9e72d48e2f05a39f7a0eb4a3a5ab37
Arthur de oliveira silva <arthuroli258@gmail.com>
2026-03-15 16:53:15 -0300

    Update dever 2.cpp

--- dever 2.cpp
+++ dever 2.cpp
@@ -5,6 +5,9 @@ int main() {
 
 	int matriz[3][3];
 	int soma = 0;
+	int par = 0, impar=0;
+	int maior,menor;
+
 
 	for(int i=0; i<3; i++) {
 		for(int j=0; j<3; j++) {
@@ -28,7 +31,7 @@ int main() {
 	for(int i=0; i<3; i++) {
 		for(int j=0; j<3; j++) {
 			if (i==j) {
-				cout << matriz[i][j]<<"";
+				cout << matriz[i][j]<<" ";
 			}
 		}
 	}
@@ -37,21 +40,54 @@ int main() {
 	for(int i=0; i<3; i++) {
 		for(int j=0; j<3; j++) {
 			if (i+j ==2) {
-				cout << matriz[i][j]<<"";
-			}
-		}
-	}
-	
-	cout << "\nPares\n";
-	for(int i=0; i<3; i++) {
-		for(int j=0; j<3; j++) {
-			if (matriz [i][j]% 2==0) {
 				cout << matriz[i][j]<<" ";
 			}
 		}
 	}
-
+	
+	cout << "\nNumeros pares :\n";
+	for(int i=0; i<3; i++) {
+		for(int j=0; j<3; j++) {
+			if (matriz [i][j]% 2==0) {
+				cout << matriz[i][j]<<" ";
+				par++;
+			}
+		}
+	}
+	
+	cout << "\nNumeros impares :\n";
+	for(int i=0; i<3; i++) {
+		for(int j=0; j<3; j++) {
+			if (matriz [i][j]% 2!=0) {
+				cout << matriz[i][j]<<" ";
+				impar++;
+			}
+		}
+	}
+	
+    maior= matriz [0][0];
+	for(int i=0; i<3; i++) {
+		for(int j=0; j<3; j++) {
+         if (matriz [i][j] > maior ){
+         	 maior = matriz [i][j];
+		   }
+        }
+     }
+     
+      menor = matriz [0][0];
+	for(int i=0; i<3; i++) {
+		for(int j=0; j<3; j++) {
+         if (matriz [i][j] < menor ){
+         	 menor = matriz [i][j];
+		   }
+        }
+     }
+    
+    
 	cout << "\nSoma da matriz: " << soma;
+	cout << "\nquantidades de Pares:\n"<< par;
+	cout << "\nquantidades de impares:\n"<< impar;
+	cout << "\nMenor valor :\n"<< menor;
+	cout << "\nMaior valor :\n"<< maior;
 
 }
-
